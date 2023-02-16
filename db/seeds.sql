8b2aaee0a0737fa73960a83f058a737fc7f0feaf
Anna <annajaninaschmidt@gmail.com>
2023-02-16 15:58:19 +1100

    schema.sql added

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+-- Department
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Finance"),
+       ("Engineering"),
+       ("Legal");
+
+-- Role
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+       ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 3),
+       ("Software Engineer", 120000, 3),
+       ("Account Manager", 160000, 2),
+       ("Accountant", 125000, 2),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 190000, 4);
+
+-- Employee  
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+-- Employee managers
+       ("Donald", "Duck", 1, NULL),
+       ("Mickey", "Mouse", 3, NULL),
+       ("Captain", "Hook", 5, NULL),
+       ("Snow", "White", 7, NULL),
+-- Employees with managers
+    --    ("Account Manager", 160000, 2),
+    --    ("Accountant", 125000, 2),
+    --    ("Legal Team Lead", 250000, 4),
+    --    ("Lawyer", 190000, 4);
+    --    ("Lawyer", 190000, 4);
+      
