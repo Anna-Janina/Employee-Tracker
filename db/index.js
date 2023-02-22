13b1273e80288c2649d263bc33c793dd25e44349
Anna <annajaninaschmidt@gmail.com>
2023-02-23 09:07:05 +1100

    viewAllEmployees

--- db/index.js
+++ db/index.js
@@ -38,7 +38,7 @@ class DB {
 
     // view employees - NOT WORKING
     viewAllEmployees() {
-        return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary")
+        return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee")
     }
 
 
